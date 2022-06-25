8e2b4c976e53ee4fe56f585c3c6e412aa047f580
vimparee <fasg901@mail.ru>
2022-06-25 14:06:08 +0500

    4

---
+++ Урок 1/04.py
@@ -0,0 +1,9 @@
+word1 = input()
+word2 = input()
+word3 = input()
+if 'раз' in word1 and 'два' in word2 and 'три' in word3:
+    print('Гори')
+elif '1' in word1 and '2' in word2 and '3' in word3:
+    print('Гори')
+else:
+    print('Не гори')
